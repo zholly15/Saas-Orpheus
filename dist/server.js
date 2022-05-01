9a1a15f5cea917cea3f23ea635a5191ce59f6bb3
zholly15 <zholly18@outlook.com>
2022-05-01 15:48:15 -0700

    Initial Commit

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const world = 'world';
+function hello() {
+    return `Hello ${world}! `;
+}
+exports.hello = hello;
+//# sourceMappingURL=server.js.map
---
+++ server.ts
@@ -0,0 +1,5 @@
+const world = 'world';
+
+export function hello(): string {
+  return `Hello ${world}! `;
+}
