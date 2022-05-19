737722f5669a73e6e6542fc9bf40bf5d3e3c5675
Albert Hunt <albert.hunt0802@gmail.com>
2022-05-19 15:13:33 -0700

    added more functionallity (edit listname) and user component

--- orpheusApp/src/app/app.module.ts
+++ orpheusApp/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListsComponent } from './lists/lists.component';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListsComponent
+    ListsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
