abae501a3a3b9ce1effebac9b32a7f708065687a
Albert Hunt <albert.hunt0802@gmail.com>
2022-05-22 13:21:53 -0700

    styling on details page

--- orpheusApp/src/app/app.module.ts
+++ orpheusApp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 import { AppComponent } from './app.component';
@@ -27,7 +29,8 @@ import { HelpComponent } from './help/help.component';
     ListDetailComponent,
     MessagesComponent,
     SearchComponent,
-    HelpComponent
+    HelpComponent,
+
 
   ],
   imports: [
@@ -39,10 +42,22 @@ import { HelpComponent } from './help/help.component';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   exports: [
-    MatIconModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
