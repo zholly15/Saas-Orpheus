7ca2dc1f70a5957bdf2ffb18365ab3bd82b6d529
Albert Hunt <albert.hunt0802@gmail.com>
2022-06-05 18:22:08 -0700

    merged my changes

--- orpheusApp/src/app/services/list.service.ts
+++ orpheusApp/src/app/services/list.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { List } from '../models/list';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { retry, catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorHandler, HandleError } from './http-error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
 
-  constructor(private httpClient : HttpClient) { }
-
+  private handleError: HandleError;
+  
   endPoint = 'http://localhost:8080/';
 
   httpOptions = {
@@ -19,8 +21,21 @@ export class ListService {
     })
   }
 
-  getLists(): Observable<List[]> {
+  constructor(private httpClient : HttpClient, httpErrorHandler: HttpErrorHandler) { 
+    this.handleError = httpErrorHandler.createHandleError('ListService')
+
+  }
+
+  public getLists(): Observable<List[]> {
     return this.httpClient.get<List[]>(this.endPoint + "lists", this.httpOptions);
+  }
+
+  public addList(list:List): Observable<List> {
+    return this.httpClient.post<List>(this.endPoint + 'lists/create/', list, this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError<List>('addList'))
+      );
   }
 
 }
